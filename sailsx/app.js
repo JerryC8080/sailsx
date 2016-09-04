13a47809d8f10b786abdf7b47f935178f7333457
JerryC <huangjerryc@gmail.com>
2016-09-04 16:04:04 +0800

    feat(dotenv): use sails-generate-x-dotenv

--- sailsx/app.js
+++ sailsx/app.js
@@ -18,6 +18,7 @@
  * `node app.js --silent --port=80 --prod`
  */
 require('babel-polyfill');
+require('dotenv').config();
 
 // Ensure we're in the project directory, so relative paths work as expected
 // no matter where we actually lift from.
