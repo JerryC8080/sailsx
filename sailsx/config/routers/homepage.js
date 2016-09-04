4ea91d5d01f098c09c6ead2e468b4048a360b849
JerryC <huangjerryc@gmail.com>
2016-09-04 17:16:26 +0800

    feat(custom routers): modify reuters folder

--- sailsx/config/routers/homepage.js
+++ sailsx/config/routers/homepage.js
@@ -36,14 +36,4 @@ module.exports.routes = {
     view: 'homepage'
   }
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
-
 };
